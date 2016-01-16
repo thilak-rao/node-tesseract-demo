d76a471358dbd6d2acae4f9bea07541ee856815b
Thilak Rao <me@thilak.in>
2016-01-16 16:15:14 +0530

    A simple multipart form upload system.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,1 +1,50 @@
+(function() {
+	'use strict';
+	var files;
+	$('#fileInput').on('change', prepareUpload);
+	$('#uploadImg').on('submit', uploadFiles);
 
+	// Grab the files and set them to our variable
+	function prepareUpload(event) {
+	  files = event.target.files;
+	}
+
+	function uploadFiles(event){
+	    event.stopPropagation(); // Stop stuff happening
+	    event.preventDefault(); // Totally stop stuff happening
+	    
+	    // Create a formdata object and add the files
+	    var data = new FormData();
+	    $.each(files, function(key, value) {
+	        data.append(key, value);
+	    });
+
+	    $.ajax({
+	        url: '/upload',
+	        type: 'POST',
+	        data: data,
+	        cache: false,
+	        dataType: 'json',
+	        processData: false, // Don't process the files
+	        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+	        success: function(data, textStatus, jqXHR) {
+	            if (typeof data.error === 'undefined') {
+	                // Success so call function to process the form
+	                uploadSuccess(event, data);
+	            } else {
+	                // Handle errors here
+	                console.log('ERRORS: ' + data.error);
+	            }
+	        },
+	        error: function(jqXHR, textStatus, errorThrown) {
+	            // Handle errors here
+	            console.log('ERRORS: ' + textStatus);
+	            // STOP LOADING SPINNER
+	        }
+	    });
+	}
+
+	function uploadSuccess(event, data) {
+		$('#successCallout').show();
+	}
+}());
--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
-
-var app = express();
+var app     = express();
+var busboy  = require('connect-busboy');
+var path    = require('path'); // used for file path
+var fs      = require('fs.extra'); // file system - for file manipulation
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({
@@ -19,6 +21,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(busboy());
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
@@ -28,8 +31,25 @@ app.use(function(req, res, next){
 });
 
 app.get('/', function(req, res) {
-	res.render('home');
+    res.render('home');
 });
+
+app.route('/upload').post(function (req, res, next) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function(fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+
+        //Path where image will be uploaded
+        fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function() {
+            console.log("Upload Finished of " + filename);
+            res.json({success: true});
+        });
+    });
+});
+
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
